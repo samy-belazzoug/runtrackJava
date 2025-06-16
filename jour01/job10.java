b403c8cccd9f72b7e3830a2cb0b4fbcc8b85d01b
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-06-16 19:54:54 +0200

    Job 10 Done.

---
+++ jour01/job10.java
@@ -0,0 +1,9 @@
+class job10 {
+    public static void main(String[]args) {
+        int facto = 1;
+        for (int i = 1; i <= 8; i++) {
+            facto*=i;
+        }
+        System.out.println("F(8) = " + facto);
+    }
+}
