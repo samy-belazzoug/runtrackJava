2e8e65753f496059c42c22b791c03f6d742ab3c8
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-06-16 19:19:56 +0200

    Job07 Done very quickly

---
+++ jour01/job07.java
@@ -0,0 +1,9 @@
+class job07 {
+    public static void main(String[]args) {
+        int somme = 0;
+        for (int i = 1;i<=100;i++) {
+            somme += i;
+        }
+        System.out.println("La somme des 100 premiers entiers est : " + somme);
+    }
+}
