15214136815eec04ff997fa1ebf1abdae5b48e10
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-06-17 10:23:12 +0200

    JOUR02 : Job00 Done.

---
+++ jour02/Individu.java
@@ -0,0 +1,13 @@
+class Individu {
+    public String nom;
+    public String prenom;
+    protected String date_de_naissance;
+    protected String lieu_de_naissance;
+    private String adresse;
+    private int telephone;
+
+    public static void main(String[]args) {
+        Individu personne = new Individu();
+        System.out.println(personne);
+    }
+}
