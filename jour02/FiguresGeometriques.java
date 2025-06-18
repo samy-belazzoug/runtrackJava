ef1859975369b9cdcd14299a2b3f3dd329dcae97
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-06-18 09:49:30 +0200

    Job 3.1 Done.

--- jour02/FiguresGeometriques.java
+++ jour02/FiguresGeometriques.java
@@ -2,20 +2,20 @@ package runtrackJava.jour02;
 
 public class FiguresGeometriques {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle(4.5, 3.0);
+        Cercle c = new Cercle(4.5, 3.0,5);
         
-        System.out.println("Longueur : " + r.getLongueur());
-        System.out.println("Largeur : " + r.getLargeur());
-        System.out.println("Surface : " + r.surface());
-
-        r.setLongueur(6.0);
-        r.setLargeur(2.0);
-
+        c.affiche();
+        System.out.println(c.surface());
+        
+        c.setX(5.0);
+        c.setY(4.0);
+        c.setRayon(8);
+        
         System.out.println("\nAprès modification :");
-        System.out.println("Longueur : " + r.getLongueur());
-        System.out.println("Largeur : " + r.getLargeur());
-        System.out.println("Surface : " + r.surface());
         
+        c.affiche();
+        System.out.println(c.surface());
+        System.out.println(c.estInterieur(3, 5));
     }
 }
 
@@ -42,6 +42,58 @@ class Rectangle {
     }
     public void setLargeur(double newLargeur) {
         largeur = newLargeur;
+    }
+
+}
+
+class Cercle {
+    private double x;
+    private double y;
+    private double rayon;
+
+    public Cercle(double x,double y, double rayon) {
+        this.x = x;
+        this.y = y;
+        this.rayon = rayon;
+    }
+    
+    public void affiche() {
+        System.out.println("Coordonnées du Cercle : " + "(" + x + "," + y + ")");
+        System.out.println("Rayon du Cercle : " + rayon);
+    }
+
+    public double surface() {
+     return rayon*Math.PI;
+    }
+
+    public boolean estInterieur(double xPoint, double yPoint) {
+        return Math.sqrt(Math.pow(xPoint-x,2) + Math.pow(yPoint-y,2)) < rayon; 
+    }
+
+    //GETTERS
+    public double getX(){
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getRayon() {
+        return rayon;
+    }
+
+    //SETTERS
+    public void setCentre(double newX, double newY) {
+        x = newX;
+        y = newY;
+    }
+    public void setX(double newX) {
+        x = newX;
+    }
+    public void setY(double newY) {
+        y = newY;
+    }
+    public void setRayon(double newRayon) {
+        rayon = newRayon;
     }
 
 }
